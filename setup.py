3a767e919182471b3e059d585a0a6345e76369ec
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-04-12 08:42:20 -0400

    Add setup.py, MANIFEST.in
    More packaging preparations. This time we're bringing in the `setup.py`
    used to install the package, and provide metadata. And the `MANIFEST.in`
    which ensures our changelog and license files are included in
    distribution files.

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+# Read the README into memory, include into package info
+cwd = path.abspath(path.dirname(__file__))
+with open(path.join(cwd, 'README.md'), 'r') as fh:
+    long_description = fh.read()
+
+
+setup(name='pywrdgen',
+      packages=find_packages(),
+      version='0.1.0',
+      author='ncdulo',
+      license='MIT',
+      url='https://github.com/ncdulo/pywrdgen',
+      keywords='password pwgen generate generator cli utility tool',
+      description='Generate (possibly) secure passwords.',
+      long_description=long_description,
+      long_description_conten_type='text/markdown',
+      python_requires='>=3.6',
+      install_requires=[
+              'click',
+          ],
+      py_modules=['cli'],
+      entry_points={
+              'console_scripts': [
+                  'pywrdgen = pywrdgen.__main__:main'
+              ],
+          },
+      classifiers=[
+              'Development Status :: 3 - Alpha',
+              'Environment :: Console',
+              'Intended Audience :: End Users/Desktop',
+              'Intended Audience :: System Administrators',
+              'License :: OSI Approved :: MIT License',
+              'Natural Language :: English',
+              'Operating System :: OS Independent',
+              'Operating System :: POSIX :: Linux',
+              'Programming Language :: Python :: 3',
+              'Programming Language :: Python :: 3.6',
+              'Programming Language :: Python :: 3.7',
+              'Programming Language :: Python :: 3.8',
+              'Programming Language :: Python :: 3 :: Only',
+              'Topic :: Security',
+              'Topic :: System :: Systems Administration',
+              'Topic :: Utilities',
+          ]
+      )
